bbf8dab3fcedbb9924f075420ccf4010fdf1854f
SID-ROG\SidAsus <sid1627@gmail.com>
2017-06-20 22:41:13 +0800

    Add Testcase CalculateFeeTest_Book1_1_Return_100

---
+++ PotterShoppingCart/PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        public class Book
+        {
+            public string Name { get; set; }
+            public int Count { get; set; }
+        }
+
+        public decimal CalculateFee(IEnumerable<Book> books)
+        {
+            return 0;
+        }  
+    }
+}
---
+++ PotterShoppingCart/PotterShoppingCartTests/ShoppingCartTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static PotterShoppingCart.ShoppingCart;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        /// <summary>
+        /// 第一集買了一本，其他都沒買，價格應為100*1=100元
+        /// </summary>
+        [TestMethod()]
+        public void CalculateFeeTest_Book1_1_Return_100()
+        {
+            //arrange
+            var target = new ShoppingCart();
+            var books = new List<Book>()
+            {
+                new Book(){
+                    Name = "HarryPotter1",
+                    Count =1
+                }
+            };
+
+            //act
+            var actual = target.CalculateFee(books);
+
+            //assert
+            var expected = 100;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
